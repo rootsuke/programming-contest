af8aeb22af892354f75c6b781d4e67fea482ebf2
rootsuke <rootsuke@gmail.com>
2023-06-04 23:56:11 +0900

    leetcode daily challenge. SearchInsertPosition

---
+++ LeetCode/Easy/SearchInsertPosition.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func searchInsert(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+
+	for left <= right {
+		mid := (left + right) / 2
+
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return left
+}
+
+func main() {
+	nums := []int{1, 3, 5, 6}
+	target := 5
+
+	fmt.Println(searchInsert(nums, target))
+}
