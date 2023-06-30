e49290a7373a87a776025caa55554a4a1aca7757
rootsuke <rootsuke@gmail.com>
2023-06-30 22:03:16 +0900

    LeetCode Daily Challenge.

---
+++ LeetCode/Easy/MiddleOfTheLinkedList.go
@@ -0,0 +1,18 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func middleNode(head *ListNode) *ListNode {
+	fast := head
+	slow := head
+
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+
+	return slow
+}
