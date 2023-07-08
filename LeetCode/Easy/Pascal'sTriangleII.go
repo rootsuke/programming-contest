fd086631cee4818266bb30426ac076db86bd6676
rootsuke <rootsuke@gmail.com>
2023-07-08 13:31:10 +0900

    LeetCode Daily Challenge.

---
+++ LeetCode/Easy/Pascal'sTriangleII.go
@@ -0,0 +1,14 @@
+package main
+
+func getRow(rowIndex int) []int {
+	row := make([]int, rowIndex+1)
+	row[0] = 1
+
+	for i := 1; i < rowIndex+1; i++ {
+		for j := i; j > 0; j-- {
+			row[j] += row[j-1]
+		}
+	}
+
+	return row
+}
